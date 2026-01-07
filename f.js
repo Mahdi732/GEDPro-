ba330eeac1cd5196465a17e8d6d44789f1f95661
Mahdi732 <ey449751@gmail.com>
2026-01-07 09:20:24 +0100

    chore: add f.js

---
+++ f.js
@@ -0,0 +1,27 @@
+// let n = 3
+// function reapeat () {
+    
+    
+//     if (n === 0 ) return;
+//     n--
+//     console.log('I love Recursion')
+    
+//     reapeat();
+// }
+// reapeat()
+//////////////////////////////////
+// function reapeat (n) {
+    
+    
+//     if (n <= 0 ) return;
+    
+//     console.log(n)
+    
+//     reapeat(n- 1);
+// }
+// reapeat(9)
+
+
+///////////////////////////////
+
+2// -
