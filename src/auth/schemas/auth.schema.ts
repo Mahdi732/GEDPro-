0ae0c1948443bcba6fb373e69f036e58bd2a9dc2
Mahdi732 <ey449751@gmail.com>
2026-01-07 09:20:52 +0100

    chore: add src/auth/schemas/auth.schema.ts

---
+++ src/auth/schemas/auth.schema.ts
@@ -0,0 +1,61 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+import { Exclude } from "class-transformer";
+
+export type UserDocument = User & Document;
+
+export enum UserRole {
+  ADMIN = "ADMIN",
+  RH = "RH",
+  MANAGER = "MANAGER",
+  USER = "USER"
+}
+
+@Schema({timestamps : true})
+export class User {
+
+    @Prop({
+    type: String,
+    default: () => uuidv4()
+  })
+  _id: string;
+
+  @Prop({
+    required: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 100
+  })
+  name: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+    trim: true
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+    minlength: 8,
+    select: false
+  })
+  @Exclude()
+  password: string;
+
+  @Prop({
+    type: String,
+    enum: UserRole,
+    default: UserRole.USER
+  })
+  role: UserRole;
+
+  @Prop({ default: false })
+  isEmailVerified: boolean;
+
+  @Prop({ default: true })
+  isActive: boolean;
+}
+
+export const userSchema = SchemaFactory.createForClass(User)
