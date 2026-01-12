1743842adfc69153be22300704c304d56c19cab0
Mahdi732 <ey449751@gmail.com>
2026-01-12 14:59:31 +0100

    Include organizationId in JWT validation

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -32,6 +32,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, role: payload.role };
+    return { userId: payload.sub, role: payload.role, organizationId: payload.organizationId };
   }
 }
